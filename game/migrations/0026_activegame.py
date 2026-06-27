3334f17d1819d8ae96bfcbbd7df83b6060fa7557
mishtiagrawal02-cloud <mishtiagrawal02@gmail.com>
2026-06-27 17:52:02 +0530

    Align ActiveGame migration with model definition

--- game/migrations/0026_activegame.py
+++ game/migrations/0026_activegame.py
@@ -3,6 +3,7 @@
 import django.db.models.deletion
 from django.conf import settings
 from django.db import migrations, models
+import django.utils.timezone
 
 
 class Migration(migrations.Migration):
@@ -18,7 +19,7 @@ class Migration(migrations.Migration):
             fields=[
                 (
                     "id",
-                    models.BigAutoField(
+                    models.AutoField(
                         auto_created=True,
                         primary_key=True,
                         serialize=False,
@@ -31,7 +32,10 @@ class Migration(migrations.Migration):
                     "status",
                     models.CharField(db_index=True, default="active", max_length=20),
                 ),
-                ("last_active", models.DateTimeField(auto_now=True)),
+                ("last_active", models.DateTimeField(
+                    default=django.utils.timezone.now,
+                    db_index=True,
+                )),
                 ("created_at", models.DateTimeField(auto_now_add=True)),
                 (
                     "user",
